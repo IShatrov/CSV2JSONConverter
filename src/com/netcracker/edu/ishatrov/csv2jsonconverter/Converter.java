6adf57c50b4bfafe81424d1cb330ac929b88907c
IShatrov <shatrov2004@mail.ru>
2024-09-12 10:22:48 +0300

    Minor refactor

--- src/com/netcracker/edu/ishatrov/csv2jsonconverter/Converter.java
+++ src/com/netcracker/edu/ishatrov/csv2jsonconverter/Converter.java
@@ -4,25 +4,21 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 
 public class Converter {
-    private static final String srcExtension = ".csv";
-    private static final String dstExtension = ".json";
+    private static final String SRC_EXTENSION = ".csv";
+    private static final String DST_EXTENSION = ".json";
 
     public static void convert(String path) {
-        File file = new File(path);
+        File file;
 
-        if (!file.isFile()) {
-            System.out.println("File " + path + " not found");
+        try {
+            file = getSrcFile(path);
+        } catch (Exception ex) {
+            System.out.println("Failed to open file " + path);
+            System.out.println(ex.getMessage());
             return;
         }
 
-        int extensionIndex = path.lastIndexOf('.');
-
-        if (!path.substring(extensionIndex).equals(srcExtension)) {
-            System.out.println(path + " is not a .csv file");
-            return;
-        }
-
-        String dstName = path.substring(0, extensionIndex) + dstExtension;
+        String dstName = getDstName(path);
 
         try (BufferedReader reader = new BufferedReader(
                 new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_16));
@@ -33,6 +29,7 @@ public class Converter {
                 line = reader.readLine();
                 System.out.println(line);
             } while (line != null);
+            
         } catch (FileNotFoundException ex) {
             System.out.println("Failed to create destination file");
             System.out.println(ex.getMessage());
@@ -40,5 +37,29 @@ public class Converter {
             System.out.println("IO error");
             System.out.println(ex.getMessage());
         }
+    }
+
+    private static int getExtensionIndex(String path) {
+        return path.lastIndexOf('.');
+    }
+
+    private static String getDstName(String path) {
+        return path.substring(0, getExtensionIndex(path)) + DST_EXTENSION;
+    }
+
+    private static File getSrcFile(String path) throws FileNotFoundException, IllegalArgumentException {
+        File file = new File(path);
+
+        if (!file.isFile()) {
+            throw new FileNotFoundException("File " + path + " not found");
+        }
+
+        int extensionIndex = getExtensionIndex(path);
+
+        if (!path.substring(extensionIndex).equals(SRC_EXTENSION)) {
+            throw new IllegalArgumentException(path + " is not a .csv file");
+        }
+
+        return file;
     }
 }
